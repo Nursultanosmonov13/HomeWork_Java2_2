52f1c7fdf67cf1edd0336f8d558c038e037c6f76
Nursultanosmonov13 <nursultanosmonov93@gmail.com>
2021-02-28 23:39:37 +0600

    ggg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        /*Asan.print();
-        Nurs.print();
-        Joni.print();
-        Beka.print();*/
-        System.out.println(createObject("1й"));
-    }
-    public static String createObject(String className){
         People Asan = new Asan("Асан","Талаев", 25,SexOfaPerson.MALE_GENDER,"Электрик","Футбол","Не женат");
         People Nurs = new Nurs("Нурсултан","Осмонов", 25,SexOfaPerson.MALE_GENDER,"Программист","Женат", 2 );
         People Joni = new Joni("Жолдошбек", "Курбашов", 32, SexOfaPerson.MALE_GENDER,"Строитель", "Боится змей");
         People Beka = new Beka("Бегимай", "Нурланбекова",18,SexOfaPerson.FEMALE,"Бухгалтер");
+
+        Asan.print();
+        Nurs.print();
+        Joni.print();
+        Beka.print();
+        System.out.println(createObject("1й"));
+    }
+    public static String createObject(String className){
 
         switch (className){
             case ("1й"):
